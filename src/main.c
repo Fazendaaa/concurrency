7701b3b86b5c7aaea7746f4b669a7d92f54d6480
farm <lukas_karotta@hotmail.com>
2017-11-06 22:32:44 -0200

    Pivotamento completo.

--- src/main.c
+++ src/main.c
@@ -1,161 +1,142 @@
 /*
 
 */
-#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <mpi.h>
 #include <omp.h>
 
-/*  ---------------------------------------------------- STRUCTS ---------------------------------------------------  */
-
-typedef struct data {
-    unsigned int start_line, chunk, exced;
-} Data;
-
-/*  ---------------------------------------------------- DEFINES ---------------------------------------------------  */
+/*  ------------------------------- DEFINES --------------------------------  */
 
 #define is_root(m)      ((0 == m) ? 1 : 0)
 #define is_tail(p, w)   ((p == w-1) ? 1 : 0)
 
-#define start(d)        (d.start_line)
-#define chunk(d)        (d.chunk)
-#define exced(d)        (d.exced)
+/*  -------------------------------- HEADERS -------------------------------  */
 
-/*  ---------------------------------------------------- HEADERS ---------------------------------------------------  */
+static int ** matrix_init (const size_t);
+static void print_matrix (int **, const size_t);
+static void free_matrix (int ***, const size_t );
 
-static int ** matrix_init (const unsigned int);
-static void print_matrix (int **, const unsigned int);
-static void free_matrix (int ***, const unsigned int);
+static void pivoting (const int, const int, int**, const size_t);
+static void merge_matrix (const int, const int, int**, const size_t);
 
-static void ring (int, char**, int **, const unsigned int);
-static void threads (int **, const unsigned int, Data, const unsigned int);
-
-/*  ----------------------------------------------------- MAIN -----------------------------------------------------  */
+/*  --------------------------------- MAIN ---------------------------------  */
 
 int main (int argc, char **argv) {
     /*  Variáveis do programa.  */
-    unsigned int matrix_size = 4;
-    int **matrix = matrix_init(matrix_size);
-   
-    ring (argc, argv, matrix, matrix_size);
+    size_t matrix_size = 4;
+    int **matrix = NULL;
+    /*  Variáveis MPI.  */
+    int world_size = 0, world_rank = 0;
+    /*  Variáveis OMP.  */
+    //int this_thread = 0, n_threads = 0;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    matrix = matrix_init(matrix_size);
+    
+    if (is_root(world_rank)) {
+        printf("\nORIGINAL MATRIX:\n");
+        print_matrix(matrix, matrix_size);
+    }
+
+    pivoting(world_rank, world_size, matrix, matrix_size);
+
+    if (is_root(world_rank)) {
+        printf("\nFINAL MATRIX:\n");
+        print_matrix(matrix, matrix_size);
+    }
+
+    MPI_Finalize();
+    
     free_matrix(&matrix, matrix_size);
 
     return 0;
 }
 
-/*  ------------------------------------------------ IMPLEMENTATIONS -----------------------------------------------  */
+/*  ---------------------------- IMPLEMENTATIONS ---------------------------  */
 
-static int ** matrix_init (const unsigned int len) {
-    int **matrix = (int**) malloc(sizeof(int *) * len);
-    unsigned int i, j;
+static int ** matrix_init (const size_t matrix_size) {
+    size_t matrix_col = matrix_size+1, matrix_line = matrix_size, i = 0, j = 0;
+    int **matrix = (int**) malloc(sizeof(int*) * matrix_line);
+    int val = 0;
 
     if (NULL != matrix) {
-        for (i = 0; i < len; i++) {
-            matrix[i] = (int*) malloc(sizeof(int) * (len+1));
-            
-            for (j = 0; j < len+1; j++)
-                matrix[i][j] = i+1;
+        for (; i < matrix_line; i++) {
+            matrix[i] = (int*) malloc(sizeof(int) * matrix_col);
+            val = i+2;
+
+            for (j = 0; j < matrix_col; j++) {
+                matrix[i][j] = val;
+            }
         }
     }
 
     return matrix;
 }
 
-static void print_matrix (int ** matrix, const unsigned int len) {
-    unsigned int i, j;
+static void print_matrix (int ** matrix, const size_t matrix_size) {
+    size_t matrix_col = matrix_size+1, matrix_line = matrix_size, i = 0, j = 0;
 
     if (NULL != matrix) {
-        for (i = 0; i < len; i++) {
-            for (j = 0; j < len+1; j++) 
+        for (; i < matrix_line; i++) {
+            for (j = 0; j < matrix_col; j++) {
                 printf("%d ", matrix[i][j]);
+            }
             printf("\n");
         }
         printf("\n");
     }
 }
 
-static void free_matrix (int *** matrix, const unsigned int len) {
-    unsigned int i;
-
-    if (NULL != matrix) {
-        for (i = 0; i < len; i++) 
+static void free_matrix (int ***matrix, const size_t size) {
+    size_t i = 0;
+    
+    if (NULL != matrix && NULL != *matrix) {
+        for (; i < size; i++) {
             free((*matrix)[i]);
-        free(*matrix);
+        }
+        free (*matrix);
         *matrix = NULL;
     }
 }
 
-static void ring (int argc, char **argv, int **matrix, const unsigned int matrix_size) {
-    unsigned int i = 0, j = 0, excess = 0;
-    int tag = 0, pivot = 0, pivot_pos = 0;
-    Data data;
-    int world_size = 0, world_rank = 0;
+static void pivoting (const int world_rank, const int world_size,
+                      int **matrix, const size_t matrix_size) {
+    size_t matrix_col = matrix_size+1, i = 0, j = 0;
+    size_t chunk = matrix_size/world_size, limit = 0, pivot = 0;
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
-    if (is_root(world_rank)) {
-        /*  Caso a matrix nao consiga ser dividida igualmente entre os processos, o ultimo arcara com o excesso.    */
-        exced(data) = matrix_size%world_size;
-        chunk(data) = matrix_size/world_size;
-        start(data) = 0;
-    } else {
-        /*  Recebe o dado anterior enviado pelo anel    */
-        MPI_Recv(&data, sizeof(Data), MPI_BYTE, world_rank-1, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
-    
-    printf("[Process %d]\nstart: %d\nchunck: %d\nexced: %d\n\n", world_rank, start(data), chunk(data), exced(data));
-    /*  Caso seja o último processo, pegar o excesso caso exista.   */
-    excess = (is_tail(world_rank, world_size)) ? 0 : exced(data);
-
-    for (i = 0; i < chunk(data)+excess; i++) {
-        pivot_pos = start(data)+i;
-        pivot = matrix[pivot_pos][pivot_pos];
-
-        printf("Pivot: %d\n", pivot);
-        /*  Divide a linha do pivo pelo seu valor.  */
-        for (j = 0; j < matrix_size+1; j++)
-            matrix[pivot_pos][j] /= pivot;
-    }
-
-    threads(matrix, matrix_size, data, is_tail(world_rank, world_size));
-    print_matrix(matrix, matrix_size);
-    
-    /*  atualiza qual vai ser a posicao inicial do próximo bloco a realizar as operacoes.   */
-    start(data) += chunk(data);        
-    MPI_Send(&data, sizeof(Data), MPI_BYTE, (world_rank+1)%world_size, tag, MPI_COMM_WORLD);
-
-    /*  Caso seja o ultimo processo do anel.    */
-    if (is_tail(world_rank, world_size)) {
-        printf("Printing matrix after all calculos:\n");
-        print_matrix(matrix, matrix_size);
-    }
-
-    MPI_Finalize();
-}
-
-static void threads (int **matrix, const unsigned int matrix_size, Data data, const unsigned int tail) {
-    unsigned int i = 0, j = 0, k = 0, excess = 0;
-    int pivot = 0, pivot_pos = 0;
-    float mult = 0;
-    
-    /*  Parte para zerar a coluna do pivo nas outras linhas.    */
-    #pragma omp parallel default(shared) private(pivot, pivot_pos)
-    {    
-        excess = (tail) ? 0 : exced(data);
+    if (NULL != matrix) {
+        /*  Calcula  até  qual  linha o processo designador será responsável por
+            pivotá-la.  */
+        limit = (world_rank+1)*chunk;
         
-        for (i = 0; i < chunk(data)+excess; i++) {
-            pivot_pos = start(data)+i;
-            pivot = matrix[pivot_pos][pivot_pos];
+        /*  Cada  processo  fica reponsável pela sua quantidade de linhas apenas
+            para pivotamento.   */
+        for (i = world_rank*chunk; i < limit; i++) {
+            pivot = matrix[i][i];
 
-            /*  pega as outras linhas sem ser a do pivot e zera a coluna.   */
-            for (j = 0; j < matrix_size; j++) {
-                mult = (float)matrix[j][pivot_pos]/(float)pivot;
-                
-                for (k = 0; k < matrix_size+1; k++)
-                    matrix[i][j] += (-mult) * matrix[j][pivot_pos];
+            /*  Como  não  há  interdependência  dos  dados  nesse  for, se pode
+                paralelizar  a  tarefa de dividir a linha pelo pivot sem maiores
+                preocupações com dependência dos valores.
+            */
+            #pragma omp parallel for
+            for (j = 0; j < matrix_col; j++) {
+                matrix[i][j] /= pivot;
             }
         }
+
+        printf("\n#%d PROCESS:\n", world_rank);
+        print_matrix(matrix, matrix_size);
     }
+}
+
+static void merge_matrix (const int world_rank, const int world_size,
+                          int **matrix, const size_t matrix_size) {
+    size_t matrix_col = matrix_size+1, i = 0, j = 0;
+    
 }
